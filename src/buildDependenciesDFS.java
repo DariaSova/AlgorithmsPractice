d6298bc96beebee7e51da656547cc49cf45b4945
Daria Sova <todariasova@gmail.com>
2018-01-13 13:22:15 -0800

    topological sort with DFS
    Change-Id: I16b88f7e4adfde18072ebcc1a50aefe9cd7c35d3

---
+++ src/buildDependenciesDFS.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class buildDependenciesDFS {
+//	ArrayList<Node> nodes;
+	HashMap<String,Node> hm;
+	
+	
+	public buildDependenciesDFS(){
+//		this.nodes = new ArrayList<>();
+		this.hm = new HashMap<>();
+	}
+
+	static class Node {
+		String x;
+		boolean isVisiting;
+		boolean visited;
+		ArrayList<Node> incoming;
+		ArrayList<Node> outgoing;
+
+		
+		public Node(String s){
+			x = s;
+			this.incoming = new ArrayList<>();
+			this.outgoing = new ArrayList<>();
+			this.isVisiting = false;
+			this.visited = false;
+		}
+	}
+	
+	Node findOrAddNode(String a) {
+		if(hm.containsKey(a)) return hm.get(a);
+		Node n = new Node(a);
+		hm.put(a,n);
+		
+		return n;
+	}
+	
+	Stack<Node> orderNodes(){
+		Stack<Node> res = new Stack<>();
+		
+		for(Node n: hm.values()){
+			if(n.isVisiting==false&&n.visited==false){
+				if(!canDFS(n, res)) return null;
+			}
+		}
+		
+		
+		return res;
+		
+	}
+	
+	boolean canDFS(Node n, Stack<Node> stack){
+		//cycle detection
+		if(n.isVisiting) return false;
+		
+		n.isVisiting = true;
+		for(Node next: n.outgoing){
+			if(!canDFS(next, stack)) return false;
+		}
+		n.visited = true;
+		stack.push(n);
+		return true;
+	}
+	
+	void addDepencency(String x, String z) {
+		Node a = findOrAddNode(x);
+		Node b = findOrAddNode(z);
+		a.outgoing.add(b);
+		b.incoming.add(a);
+}
+	
+	public static void main(String[] args){
+		buildDependenciesDFS myBuild = new buildDependenciesDFS();
+		//add dependencies
+		myBuild.addDepencency("a", "b");
+		myBuild.addDepencency("a", "d");
+		myBuild.addDepencency("b", "z");
+		myBuild.addDepencency("d", "x");
+		myBuild.addDepencency("x", "m");
+		//find order
+		Stack<Node> res = myBuild.orderNodes();
+		//print
+		while(!res.isEmpty()) System.out.println(res.pop().x +" ");
+		
+	}
+}
