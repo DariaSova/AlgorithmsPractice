7bf62a959d63074baaf0822d1a0dc339509fee45
Daria Sova <todariasova@gmail.com>
2018-01-14 19:22:35 -0800

    Print the first path for findGrid
    Change-Id: Ie439e549fce9042e2b9775b9cd9e5b1e99e3be37

--- src/gridPath.java
+++ src/gridPath.java
@@ -1,21 +1,45 @@
+import java.util.*;
+
 public class gridPath {
+		static class Point{
+			int x;
+			int y;
+			
+			public Point(int x, int y){
+				this.x = x;
+				this.y = y;
+			}
+		}
 	
-		public static boolean findPathRecursive(int[][] matrix) {
-			return 	findPath(matrix, 0, 0);
+		public static ArrayList<Point> findPathRecursive(int[][] matrix) {
+			ArrayList<Point> res = new ArrayList<Point>();
+			if(findPath(matrix, res, 0, 0)) return res;
+			//better return null and check in main
+			return new ArrayList<Point>();
 		}
 		
-		public static boolean findPath(int[][] matrix, int r, int c) {
-			if(r>=matrix.length||c>=matrix[0].length) return false;
-			if(r==matrix.length-1&&c==matrix[0].length-1)
+		
+		//TODO: write another method that will return all possible paths
+		public static boolean findPath(int[][] matrix, ArrayList<Point> res, int r, int c) {
+			if(r==matrix.length||c==matrix[0].length) return false;
+			Point curr = new Point(r,c);
+			if(r==matrix.length-1&&c==matrix[0].length-1){
+				res.add(curr);
 				return true;
+			}
+			
 			if(matrix[r][c]==-1) return false;
 			
-			return findPath(matrix, r, c+1) || findPath(matrix, r+1, c);
+			
+			res.add(curr);
+			
+			return findPath(matrix, res, r, c+1)||findPath(matrix, res, r+1, c);
 		}
 		
 		public static void main(String[] args) {
-			int[][] grid = {{0,0,-1,-1},{0,0,-1,0},{-1,0,-1,-1},{-1,-1,-1,0}};
-			System.out.println(findPathRecursive(grid));
+			int[][] grid = {{0,-1,-1,-1},{0,-1,-1,0},{0,0,0,-1},{-1,-1,0,0}};
+			for(Point p: findPathRecursive(grid))
+					System.out.println(p.x +" "+p.y+"\n");
 		}
 }
 
