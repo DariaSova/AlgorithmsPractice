4495a34c188a3dec9ebb023c6e051e9c33bce660
Daria Sova <todariasova@gmail.com>
2018-01-14 20:03:07 -0800

    Print all paths in gridPath: recursive
    Change-Id: I2b59b96fc2e6ceec81c5f4e0e4d8cc8c02ceb963

--- src/gridPath.java
+++ src/gridPath.java
@@ -18,8 +18,36 @@ public class gridPath {
 			return new ArrayList<Point>();
 		}
 		
+		public static ArrayList<ArrayList<Point>> findAllPathsRecursive(int[][] matrix) {
+			ArrayList<ArrayList<Point>> res = new ArrayList<ArrayList<Point>>();
+			ArrayList<Point> path = new ArrayList<Point>();
+			findAllPaths(matrix, res, path, 0, 0);
+			//better return null and check in main
+			return res;
+		}
 		
-		//TODO: write another method that will return all possible paths
+		
+		public static void findAllPaths(int[][] matrix, ArrayList<ArrayList<Point>> res, ArrayList<Point> currPath, int r, int c){
+			ArrayList<Point> newPath = (ArrayList<Point>) currPath.clone();
+			Point curr = new Point(r,c);
+			if(r==matrix.length||c==matrix[0].length) return;
+			
+			if(r==matrix.length-1&&c==matrix[0].length-1){
+				newPath.add(curr);
+				res.add(newPath);
+				return;
+			}
+			
+			if(matrix[r][c]==-1) return;
+			
+			newPath.add(curr);
+			
+			findAllPaths(matrix, res, newPath, r, c+1);
+			findAllPaths(matrix, res, newPath, r+1, c);
+			
+		}
+		
+		
 		public static boolean findPath(int[][] matrix, ArrayList<Point> res, int r, int c) {
 			if(r==matrix.length||c==matrix[0].length) return false;
 			Point curr = new Point(r,c);
@@ -38,8 +66,15 @@ public class gridPath {
 		
 		public static void main(String[] args) {
 			int[][] grid = {{0,-1,-1,-1},{0,-1,-1,0},{0,0,0,-1},{-1,-1,0,0}};
-			for(Point p: findPathRecursive(grid))
+			int[][] emptyGrid = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+//			for(Point p: findPathRecursive(grid))
+//					System.out.println(p.x +" "+p.y+"\n");
+			for(ArrayList<Point> l: findAllPathsRecursive(emptyGrid)){
+				for(Point p: l)
 					System.out.println(p.x +" "+p.y+"\n");
+				System.out.println("=========");
+			}
+				
 		}
 }
 
