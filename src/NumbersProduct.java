30aa4d1a7f43387e0979924a4f12dff9169115ad
Daria Sova <todariasova@gmail.com>
2018-01-15 14:36:13 -0800

    Compute nums product without multiplication
    Change-Id: Id56783df481ef58132f54b1c99c1462a6ffbbf20

---
+++ src/NumbersProduct.java
@@ -0,0 +1,37 @@
+
+public class NumbersProduct {
+	
+	public static int computeProduct(int a, int b){
+		int smaller = a < b ? a : b;
+		int bigger = a >= b ? a : b;		
+		int res = multiply(a, b);
+
+		return res;
+	}
+
+	public static int multiply(int s, int b) {
+		if(s==0) return 0;
+		if(s==1) return b;
+		if(s==-1) return b-b-b;
+		
+		int smaller = s >> 1; //divide by two
+		int res1 = multiply(smaller,b);
+		
+		if(s % 2 ==0) { //if even
+			return 2*res1;
+		} else {
+			return 2*res1 + b;
+		}
+		
+	}
+	
+	
+	public static void main(String[] args){
+		System.out.println(computeProduct(0,1));
+		System.out.println(computeProduct(5,1));
+		System.out.println(computeProduct(3,2));
+		System.out.println(computeProduct(10,20));
+		System.out.println(computeProduct(-1, 4));
+		System.out.println(computeProduct(-1, -4));
+	}
+}
