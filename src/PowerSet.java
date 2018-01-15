821ce04afc15fdcc80ee1dd0bbcdeebcdb3db6be
Daria Sova <todariasova@gmail.com>
2018-01-15 14:01:48 -0800

    Add binary number implementation for powerset
    Change-Id: I0fb26475af8619ba835fd52b15387555b02a185b

--- src/PowerSet.java
+++ src/PowerSet.java
@@ -48,16 +48,66 @@ public class PowerSet {
 		return allsets;
 	}
 	
+	//index starts at 0
+	public static ArrayList<ArrayList<Integer>> anotherPowerSet(ArrayList<Integer> set, int index) {
+		ArrayList<ArrayList<Integer>> res;
+		if(set.size()==index){ //base case
+			res = new ArrayList<ArrayList<Integer>>();
+			res.add(new ArrayList<Integer>()); //add empty list
+		} else{
+			res = anotherPowerSet(set, index+1);
+			int item = set.get(index);
+			ArrayList<ArrayList<Integer>> morelists = new ArrayList<ArrayList<Integer>>(); 
+			for(ArrayList<Integer> list: res){
+				ArrayList<Integer> newlist = new ArrayList<Integer>();
+				newlist.addAll(list);
+				newlist.add(item);
+				morelists.add(newlist);
+			}
+			res.addAll(morelists);
+		}
+		
+		return res;
+	}
+	
+	public static ArrayList<ArrayList<Integer>> powerSetBinary(ArrayList<Integer> set) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+		int max = 1 << set.size();
+		
+		for(int k=0; k<max; k++){
+			ArrayList<Integer> newlist = getList(set, k);
+			res.add(newlist);
+		}
+		
+		return res;
+	}
+	
+	public static ArrayList<Integer> getList(ArrayList<Integer> set, int k){
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		
+		for(int i=0; i<k; i<<=1){
+			if((i&k)==1){
+				res.add(set.get(i));
+			}
+		}
+		return res;
+	}
+	
 	public static void main(String[] args) {
 		ArrayList<Integer> nums = new ArrayList<Integer>();
 		nums.add(1);
-		nums.add(2);
 		nums.add(3);
+		nums.add(5);
 		
 		ArrayList<ArrayList<Integer>> res = getSets(nums, 0);
 		System.out.println(res.toString());
 		ArrayList<ArrayList<Integer>> res2 = getSets2(nums, nums.size()-1);
 		System.out.println(res2.toString());
+		ArrayList<ArrayList<Integer>> res3 = anotherPowerSet(nums, 0);
+		System.out.println(res3.toString());
+		
+		ArrayList<ArrayList<Integer>> res4 = powerSetBinary(nums);
+		System.out.println(res4.toString());
 
 	}
 }
