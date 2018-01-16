5af69d93a7e4cf3a199f348a02ba625c2ab285d2
Daria Sova <todariasova@gmail.com>
2018-01-15 17:22:46 -0800

    Add all permutations of a String
    Change-Id: I080c2e0c17d3c5833def820c0374b7f5e769db1b

--- src/AllStringPermutations.java
+++ src/AllStringPermutations.java
@@ -1,4 +1,44 @@
+import java.util.*;
 
 public class AllStringPermutations {
-
+	
+	public static HashSet<String> findPermutations(String s){
+		if(s.length()==0) return null;
+//		if(s.length()==1) can make it a special case
+		return getPermutations(s.toCharArray(), 0);
+	}
+	
+	public static HashSet<String> getPermutations(char[] in, int index){
+		HashSet<String> result;
+		if(index == in.length-1) {
+			 result =  new HashSet<String>();
+			 result.add(String.valueOf(in[index]));
+		}else{
+			result = getPermutations(in, index+1);
+			char curr = in[index];
+			
+			HashSet<String> new_result = new HashSet<String>();
+			//try having array list of string builders
+			for(String s: result){
+				for(int i=0; i<=s.length(); i++){
+					//check last index
+					String newstr = s.substring(0,i) + curr + s.substring(i);
+					new_result.add(newstr);
+				}
+			}
+			result = new_result;
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		String s1 = "ABC";
+		String s2 = "ABCD";
+		HashSet<String> res1 = findPermutations(s1);
+		System.out.println(res1.toString());
+		HashSet<String> res2 = findPermutations(s2);
+		System.out.println(res2.toString());
+	}
 }
+
+
